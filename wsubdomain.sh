d344332fe075fd83760eca7aa52e5a3c04da2205
wuseman <wuseman@nr1.nu>
2020-07-12 10:47:12 +0100

    quotes+bash=true - added quotes :)

---
+++ wsubdomain.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+###############################################################################
+###############################################################################
+###                                                                         ###
+### Author: wuseman <wuseman@nr1.nu>                                        ###
+### IRC: Freenode @ wuseman                                                 ###
+###                                                                         ###
+###############################################################################
+############################## AUTHOR WUSMAN ##################################
+###############################################################################
+###                                                                         ###
+### If you will copy any developers work and claim you are the dev/founder  ###
+### it wont make you a hacker - The only person you're fooling is yourself  ###
+### so please respect all developers and GPL Licenses no matter if it's my  ###
+### script, tool or project or if it's anyone else, thank you!              ###
+###                                                                         ###
+###############################################################################
+###########################o###################################################
+###############################################################################
+####                                                                       ####
+####  Copyright (C) 2018-2020, wuseman                                     ####
+####                                                                       ####
+####  This program is free software; you can redistribute it and/or modify ####
+####  it under the terms of the GNU General Public License as published by ####
+####  the Free Software Foundation; either version 2 of the License, or    ####
+####  (at your option) any later version.                                  ####
+####                                                                       ####
+####  This program is distributed in the hope that it will be useful,      ####
+####  but WITHOUT ANY WARRANTY; without even the implied warranty of       ####
+####  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        ####
+####  GNU General Public License for more details.                         ####
+####                                                                       ####
+####  You must obey the GNU General Public License. If you will modify     ####
+####  the file(s), you may extend this exception to your version           ####
+####  of the file(s), but you are not obligated to do so.  If you do not   ####
+####  wish to do so, delete this exception statement from your version.    ####
+####  If you delete this exception statement from all source files in the  ####
+####  program, then also delete it here.                                   ####
+####                                                                       ####
+###############################################################################
+###############################################################################
+### Modified: 09:27:36 - 2020-07-12
+
+SITE="${1}"
+if [[ -z ${SITE} ]]; then
+     echo -e "internal error -- usage example: ./$basename$0 domain.nu"
+     exit 1
+fi
+
+echo -e "--------------------------------------------"
+echo -e "-> Searching subdomains for \e[4m$SITE\e[0m."
+echo -e "--------------------------------------------"
+curl -fsSL "https://crt.sh/?CN=%25.${SITE}&exclude=expired" \
+    |grep -i ${SITE} \
+    |sed 's/<TD>//;s/<\/TD>//g' \
+    |awk '!seen[$0]++' \
+    |grep "${1}$" \
+    |sed -e 's/^[ \t]*//' \
+    |awk '{print NR  "->  " $s}' > "./.wsubdomain"
+if [[ "$(cat .wsubdomain|wc -l)"  -lt "1" ]]; then
+     echo -e "-> $SITE has no certificate..."
+     echo -e "--------------------------------------------\n"
+     exit 1
+else
+     cat "./.wsubdomain"
+     echo -e "--------------------------------------------"
+     echo -e "-> Found $(cat .wsubdomain|wc -l) subdomain(s)"
+     echo -e "--------------------------------------------"
+     rm "./.wsubdomain"
+fi
